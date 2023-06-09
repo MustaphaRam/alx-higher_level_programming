3f3d5b0a6cb6fe26cd976f51f7aad2d97eb1d6aa
MustaphaRam <mustapha.mad.75@gmail.com>
2023-06-09 17:08:58 +0100

    0x03 Python - Data Structures: Lists, Tuples

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,23 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - prints information about a Python list object
+ * @p: pointer to the Python list object
+ */
+void print_python_list_info(PyObject *p)
+{
+	Py_ssize_t size, i;
+	PyObject *item, *type;
+
+	size = PyList_Size(p);
+	printf("[*] Size of the Python List = %ld\n", size);
+
+	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GetItem(p, i);
+		type = Py_TYPE(item);
+		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
+	}
+}
