f6443194814b73e1d3a80042715951ca0f867f00
MustaphaRam <mustapha.mad.75@gmail.com>
2023-06-10 12:37:43 +0100

    0x03 Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 def no_c(my_string):
-    """Remove all characters c and C from a string."""
-    copy = [x for x in my_string if x != 'c' and x != 'C']
-    return ("".join(copy))
+    if my_string:
+        str = ""
+        for c in my_string:
+            if c != 'c' and c != 'C':
+                str += c
+        return str
