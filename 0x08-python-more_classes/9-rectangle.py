959429252ddb5cd1ae2b25269efeef4d10c82033
MustaphaRam <mustapha.mad.75@gmail.com>
2023-07-03 19:36:47 +0100

    tasks 0x08. Python - More Classes and Objects

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -65,7 +65,8 @@ class Rectangle:
         """
         if self.__width == 0 or self.__height == 0:
             return ""
-        return '\n'.join([str(self.print_symbol) * self.__width] * self.__height)
+        symbol = str(self.print_symbol)
+        return '\n'.join([symbol * self.__width] * self.__height)
 
     def __repr__(self):
         """Return the string representation of the Rectangle."""
