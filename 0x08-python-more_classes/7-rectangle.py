6bcee0b48baba2e5024d75d226b53ee16ae4f821
MustaphaRam <mustapha.mad.75@gmail.com>
2023-07-03 19:26:47 +0100

    tasks 0x08. Python - More Classes and Objects

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -7,7 +7,7 @@ class Rectangle:
 
     """Attributes:
         number_of_instances (int): The number of Rectangle instances.
-        print_symbol (any): The symbol used for string representation.
+        print_symbol (string): The symbol used for string representation.
     """
 
     number_of_instances = 0
