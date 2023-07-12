0fc8d0e27cd45dee67bb3668b292a35421d8c616
MustaphaRam <mustapha.mad.75@gmail.com>
2023-07-12 06:04:10 +0100

    0x0B-python-input_output

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,26 +1,37 @@
 #!/usr/bin/python3
-"""Module 100-append_after.
-Inserts a line of text to a file,
-after each line containing a specific string.
-"""
+"""Defines a class Student."""
 
 
-def append_after(filename="", search_string="", new_string=""):
-    """Appends the new_string after
-    the search_string in filename.
-    Args:
-        - filename: name of the file
-        - search_string: string to append after
-        - new_string: new_string to append
-    """
+class Student:
+    """Represent a student."""
 
-    with open(filename, "r") as f:
-        text = f.readlines()
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-    with open(filename, "w") as fo:
-        s = ""
-        for line in text:
-            s += line
-            if search_string in line:
-                s += new_string
-        fo.write(s)
+    def to_json(self, attrs=None):
+        """Get a dictionary representation of the Student.
+        If attrs is a list of strings, represents only those attributes
+        included in the list.
+        Args:
+            attrs (list): (Optional) The attributes to represent.
+        """
+        if (type(attrs) == list and
+                all(type(ele) == str for ele in attrs)):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the Student.
+        Args:
+            json (dict): The key/value pairs to replace attributes with.
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
