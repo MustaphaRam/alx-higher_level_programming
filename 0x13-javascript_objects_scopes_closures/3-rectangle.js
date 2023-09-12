42f0e4ddfdf3b5b8382769d0f419a8c8ccebebf9
MustaphaRam <mustapha.mad.75@gmail.com>
2023-09-12 19:48:04 +0100

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || typeof w !== 'number' || typeof h !== 'number') {
-      return {};
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
-
-    this.width = w;
-    this.height = h;
   }
 
   print () {
