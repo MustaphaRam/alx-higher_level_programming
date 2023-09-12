c294acc47c24e236873ca6dbcf3182a94a7fa076
MustaphaRam <mustapha.mad.75@gmail.com>
2023-09-12 20:32:48 +0100

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const list = require('./test/100-data.js').list;
+const list = require('./100-data.js').list;
 const arr = list.map((x, i) => x * i);
 console.log(list);
 console.log(arr);
--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const { dict } = require('./test/101-data');
+const { dict } = require('./101-data');
 
 const myValue = Object.entries(dict).reduce((acc, [key, value]) => {
   acc[value] = acc[value] ? [...acc[value], key] : [key];
