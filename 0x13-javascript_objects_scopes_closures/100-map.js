3b630ca6b4aaccb1bf227e3dfbd8471f8eb771ad
MustaphaRam <mustapha.mad.75@gmail.com>
2023-09-12 20:30:18 +0100

    0x13-javascript_objects_scopes_closures

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const list = require('./test/100-data.js').list;
+const arr = list.map((x, i) => x * i);
 console.log(list);
-console.log(list.map((item, index) => item * index));
+console.log(arr);
