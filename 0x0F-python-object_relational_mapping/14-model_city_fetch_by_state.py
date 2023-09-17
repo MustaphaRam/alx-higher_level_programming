f0d66e56f863691ed8c4a8a9744b43c3ce0a4266
MustaphaRam <mustapha.mad.75@gmail.com>
2023-09-17 17:59:31 +0100

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -16,10 +16,10 @@ if __name__ == '__main__':
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
-
     session = Session(engine)
+
     for state, city in session.query(State, City)\
                               .filter(City.state_id == State.id)\
                               .order_by(City.id).all():
-            print("{}: ({}) {}".format(state.name, city.id, city.name))
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
     session.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,11 +5,11 @@ import MySQLdb
 import sys
 
 if __name__ == '__main__':
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                        passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
-                ORDER BY states.id ASC".format(sys.argv[4]))
+                   ORDER BY states.id ASC".format(sys.argv[4]))
     [print(state) for state in cursor.fetchall()]
     cursor.close()
     db.close()
