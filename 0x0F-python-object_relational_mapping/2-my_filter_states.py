bc328a88b3794a4b1586f7b0d2dd14be228c54d7
MustaphaRam <mustapha.mad.75@gmail.com>
2023-09-17 12:59:33 +0100

    0x0F. Python - Object-relational mapping

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""script that takes in an argument and displays all values in the states table of hbtn_0e_0_usa where name matches the argument"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM `states` where name LIKE BINARY 'N%' ORDER BY states.id")
+    [print(state) for state in cursor.fetchall()]
+    cursor.close()
+    db.close()
