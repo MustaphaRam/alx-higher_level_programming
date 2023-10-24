77e0f7c850a5dd8cb0e7b05135995a659b28b9d6
MustaphaRam <mustapha.mad.75@gmail.com>
2023-10-24 15:20:39 +0100

    0x14. JavaScript - Web scraping

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,17 +7,19 @@ let num = 0;
 request(url, function (err, response, body) {
   if (err) {
     console.log(err);
-  } else if (response.statusCode === 200) {
-    const content = JSON.parse(body);
-    content.results.forEach((film) => {
-      film.characters.forEach((character) => {
-        if (character.includes(18)) {
-          num += 1;
-        }
-      });
-    });
-    console.log(num);
   } else {
-    console.log('code err ' + response.statusCode);
+    if (response.statusCode === 200) {
+      const content = JSON.parse(body);
+      content.results.forEach((film) => {
+        film.characters.forEach((character) => {
+          if (character.includes(18)) {
+            num += 1;
+          }
+        });
+      });
+      console.log(num);
+    } else {
+      console.log('code err ' + response.statusCode);
+    }
   }
 });
