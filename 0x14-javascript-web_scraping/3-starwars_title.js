33c002dffcb335916c3f70b020a910a51f000361
MustaphaRam <mustapha.mad.75@gmail.com>
2023-10-24 14:52:16 +0100

    0x14. JavaScript - Web scraping

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-request(url, function (err, response, body) {
+request.get(url, (err, response, body) => {
   if (err) {
     console.log(err);
   } else {
