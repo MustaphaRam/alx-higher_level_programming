adbef34ac698912c33a3b80efe0cc52a7be1af46
MustaphaRam <mustapha.mad.75@gmail.com>
2023-10-24 15:59:51 +0100

    0x14. JavaScript - Web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -3,7 +3,6 @@
 const request = require('request');
 const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
 
-
 function displayCharacters (characters, index) {
   request(characters[index], function (error, response, body) {
     if (!error) {
