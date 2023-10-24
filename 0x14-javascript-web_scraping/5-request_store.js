87b3dfadc62c07b69e0601094b48a6edb64af827
MustaphaRam <mustapha.mad.75@gmail.com>
2023-10-24 13:45:05 +0100

    0x14. JavaScript - Web scraping

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, function (err, response, body) {
+    if (err)
+        console.log(err);
+    else {
+        if (response.statusCode === 200){
+            fs.writeFile(file, body, 'utf-8', (error) => {
+                if (error) {
+                  console.log(error);
+                }
+            });
+        }
+        else
+            console.log('code err '+ response.statusCode);
+    }
+});
