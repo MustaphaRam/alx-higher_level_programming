563530cee5dfab9e4ad8cb309f1fcaeaa8cb9657
MustaphaRam <125461674+MustaphaRam@users.noreply.github.com>
2023-12-20 17:15:26 +0100

    Update 6-main.py

--- 0x10-python-network_0/6-main.py
+++ 0x10-python-network_0/6-main.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-""" Test function find_peak """
-find_peak = __import__('6-peak').find_peak
+"""Algorithms for list of integers."""
 
-print(find_peak([1, 2, 4, 6, 3]))
-print(find_peak([4, 2, 1, 2, 3, 1]))
-print(find_peak([2, 2, 2]))
-print(find_peak([]))
-print(find_peak([-2, -4, 2, 1]))
-print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
+
+def find_peak(list_of_integers):
+    """Finds a peak in a list of unsorted integers."""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
